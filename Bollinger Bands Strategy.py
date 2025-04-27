5bd6bf91832d47fe7ce486fce929c5f6653a095a
jialin zheng <94853705+yuanlaik@users.noreply.github.com>
2025-04-27 13:39:04 +0800

    Add files via upload

---
+++ Bollinger Bands Strategy.py
@@ -0,0 +1,12 @@
+#%% md
+# # This is a sample Jupyter Notebook
+# 
+# Below is an example of a code cell. 
+# Put your cursor into the cell and press Shift+Enter to execute it and select the next one, or click 'Run Cell' button.
+# 
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+# 
+# To learn more about Jupyter Notebooks in PyCharm, see [help](https://www.jetbrains.com/help/pycharm/ipython-notebook-support.html).
+# For an overview of PyCharm, go to Help -> Learn IDE features or refer to [our documentation](https://www.jetbrains.com/help/pycharm/getting-started.html).
+#%%
+print("Hello World!")
